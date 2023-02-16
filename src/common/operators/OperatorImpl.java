cda56bd2bc0e3b6e489ba29714209add1ad07ba4
Madhuri Tayade <mt202202@appystack.com>
2023-02-16 16:05:48 +0530

    Variables

---
+++ src/common/operators/OperatorImpl.java
@@ -0,0 +1,15 @@
+package common.operators;
+
+public class OperatorImpl
+{
+    public static void main(String[] args) {
+        //Unary Operator
+        int a = 50;
+        System.out.println("a++:"+ a++); //50
+        System.out.println("++a"+ ++a);  //52
+        int b= 10;
+        System.out.println("b--"+ b--); //10
+        System.out.println("--b"+ --b); //8
+
+    }
+}
