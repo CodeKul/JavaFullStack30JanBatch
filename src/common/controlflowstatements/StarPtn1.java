d19f613fd9fdce0175593f65bac37dc9d67e909a
Madhuri Tayade <mt202202@appystack.com>
2023-03-13 16:35:56 +0530

    Nested for loop

---
+++ src/common/controlflowstatements/StarPtn1.java
@@ -0,0 +1,18 @@
+package common.controlflowstatements;
+
+/**
+  * * *
+  * * *
+  * * *
+
+ */
+public class StarPtn1 {
+    public static void main(String[] args) {
+        for (int i=0;i<3;i++){
+            for (int j=0;j<3;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
