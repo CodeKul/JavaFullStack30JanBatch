ebe3707b7f4274d579a2727ccc4cb3b6b9d00a89
Madhuri Tayade <mt202202@appystack.com>
2023-03-27 16:24:13 +0530

    objects & classes

---
+++ src/oops/Student.java
@@ -0,0 +1,19 @@
+package oops;
+
+public class Student {
+    String name;
+    String prn;
+    int rollNo;
+
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "Rishi";
+        s1.prn ="RKFL5665";
+        s1.rollNo = 55;
+
+        Student s2 = new Student();
+        s2.name = "Varsha";
+        s2.prn ="RKFL5663";
+        s2.rollNo = 51;
+    }
+}
