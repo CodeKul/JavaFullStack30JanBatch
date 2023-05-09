854046301be1c93a891d3430c0760fba2c77ac38
Madhuri Tayade <mt202202@appystack.com>
2023-05-09 15:14:19 +0530

    interface introduction

---
+++ src/interface_block/Showable.java
@@ -0,0 +1,18 @@
+package interface_block;
+
+public interface Showable {
+    void show();
+}
+class TestInterface implements Showable{
+    @Override
+    public void show() {
+        System.out.println("in show method");
+    }
+}
+
+class ShowableImpl{
+    public static void main(String[] args) {
+        Showable showable = new TestInterface();
+        showable.show();
+    }
+}
