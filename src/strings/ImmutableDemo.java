25848cefd89cb4eeb641c8729e266ad9738b2941
Madhuri Tayade <mt202202@appystack.com>
2023-05-30 16:30:02 +0530

    string class immutable

---
+++ src/strings/ImmutableDemo.java
@@ -0,0 +1,12 @@
+package strings;
+
+public class ImmutableDemo {
+    public static void main(String[] args) {
+        String s1 = "Spring";
+        String s2 = "Java";
+        System.out.println(s1.concat(s2));
+        System.out.println(s1);
+        System.out.println();
+
+    }
+}
---
+++ src/strings/StringDemo.java
@@ -0,0 +1,9 @@
+package strings;
+
+public class StringDemo {
+    String msg = "Welcome";
+    char msgChar[] ={'W','e','l','c','o','m','e'};
+    String msgString = new String(("Welcome"));
+    public static void main(String[] args) {
+    }
+}
