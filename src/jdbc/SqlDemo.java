0ada96cf98645bc8ae440c726dd57c0f66b7f28e
Madhuri Tayade <mt202202@appystack.com>
2023-06-23 16:43:36 +0530

    MySql

---
+++ src/jdbc/SqlDemo.java
@@ -0,0 +1,9 @@
+package jdbc;
+import java.sql.*;
+
+
+public class SqlDemo {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+    }
+}
