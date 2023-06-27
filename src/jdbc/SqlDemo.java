2974b34a6fbdded540e3e1a21c64172a0baca8fd
Madhuri Tayade <mt202202@appystack.com>
2023-06-27 16:37:43 +0530

    Jdbc

--- src/jdbc/SqlDemo.java
+++ src/jdbc/SqlDemo.java
@@ -1,9 +1,32 @@
 package jdbc;
+
 import java.sql.*;
 
-
 public class SqlDemo {
-    public static void main(String[] args) throws ClassNotFoundException {
-        Class.forName("com.mysql.jdbc.Driver");
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        String url ="jdbc:mysql://localhost:3306/Java30Jan";
+        String username="root";
+        String password="Codekul@123";
+       Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con =DriverManager.getConnection(url,username,password);
+        Statement st = con.createStatement();
+       ResultSet resultSet =  st.executeQuery("select  * from student");
+       while (resultSet.next()){
+           System.out.println("---------Student---------------");
+           System.out.println("Roll No "+resultSet.getInt(1));
+           System.out.println("Name No "+resultSet.getString(2));
+           System.out.println("Age "+resultSet.getInt(3));
+       }
+       con.close();
     }
 }
+
+//class Demo{
+//    {
+//        System.out.println("in instance block");
+//    }
+//    static {
+//        System.out.println("in static block");
+//    }
+//}
