8d414f8add2e8c09f1228069a24edb220022ea7b
Madhuri Tayade <mt202202@appystack.com>
2023-08-03 17:05:38 +0530

    Vector

---
+++ src/arrays/StarPattern3.java
@@ -0,0 +1,20 @@
+package arrays;
+
+public class StarPattern3 {
+    public static void main(String[] args) {
+        int i, j, row=5;
+
+        for (i=1; i<=row; i++) {
+
+            for (j=1; j<=i; j++)
+            {
+
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+
---
+++ src/collection/VectorDemo.java
@@ -0,0 +1,24 @@
+package collection;
+
+import java.util.Vector;
+
+public class VectorDemo {
+    public static void main(String[] args) {
+        Vector<String> cities = new Vector<>();
+        cities.add("Pune");
+        cities.add("Delhi");
+        cities.add("Noida");
+        cities.add("Nagpur");
+        cities.add("Mumbai");
+        cities.add("Madras");
+        cities.add("Solapur");
+        cities.add("Goa");
+        cities.add("Hyderabad");
+        cities.add("Chennai");
+        System.out.println("Capacity: "+cities.capacity());
+        cities.addElement("Nagpur");
+        for (String city:cities){
+            System.out.println("city "+city);
+        }
+    }
+}
